337870548c4b2258e0078728cb6220d48e785d9d
kem <531144129@qq.com>
2025-01-02 17:34:39 +0800

    恢复 ErrorCode

--- internal/service/emby/schema.go
+++ internal/service/emby/schema.go
@@ -7,7 +7,7 @@ type EmbyResponse struct {
 
 // /Items/:itemID/PlaybackInfo的响应
 type PlaybackInfoResponse struct {
-	// ErrorCode     *PlaybackErrorCode `json:"ErrorCode,omitempty"`
+	ErrorCode     *PlaybackErrorCode `json:"ErrorCode,omitempty"`
 	MediaSources  []MediaSourceInfo  `json:"MediaSources,omitempty"`
 	PlaySessionID *string            `json:"PlaySessionId,omitempty"`
 }
